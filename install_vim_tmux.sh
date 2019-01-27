9647546bc13b56057a764d3994a4eb6bcec84c8b
gpid007 <heine.gregor@gmail.com>
2019-01-27 15:28:20 +0100

    rm log

--- install_vim_tmux.sh
+++ install_vim_tmux.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 set -e
 
-
-sudo echo -e "\n# ====================== #\n#    INSTALL VIM TMUX    #\n# ====================== #" > $LOG
+echo -e "# ====================== #\n#    INSTALL VIM TMUX    #\n# ====================== #"
 VIM=~/vim
 VDOT=~/.vim
 GIT_BASH=~/.bash-git-prompt
 DIR_ARR=($VIM $VDOT $GIT_BASH)
 OWN_PATH=$(dirname "$relpath $0")
-LOG=$OWN_PATH/log_install
 
 
-echo -e "\n$(date +%Y-%m-%d' '%T) \n# ========== OS-RELEASE INSTALL ========== #" | tee -a $LOG
+echo "# ========== OS-RELEASE INSTALL ========== #"
 if [ $(which yum) ]; then
     sudo yum install -y vim git gcc gcc-c++ ctags cmake python3-devel golang ncurses-devel tmux
 else
     sudo apt install -y vim git gcc g++ ctags cmake python3-dev golang-go ncurses-dev tmux
 fi
-echo "Success" | tee -a $LOG
 
 
-echo -e "\n$(date +%Y-%m-%d' '%T) \n# ========== GIT CLONE ========== #" | tee -a $LOG
+echo "# ========== GIT CLONE ========== #"
 cd $HOME
 for dir in ${DIR_ARR[@]}; do
     if [ -d "$dir" ]; then
@@ -38,10 +35,8 @@ git clone https://github.com/lifepillar/vim-solarized8.git $VDOT/pack/themes/opt
 git clone https://github.com/itchyny/lightline.vim $VDOT/bundle/lightline.vim
 # git clone https://github.com/Rip-Rip/clang_complete.git $VDOT/clang_complete
 
-echo "Success" | tee -a $LOG
 
-
-echo -e "\n$(date +%Y-%m-%d' '%T) \n# ========== VIM 8 ========== #" | tee -a $LOG
+echo "# ========== VIM 8 ========== #"
 
 # ---------- VIM 8 INSTALL ---------- #
 cd $VIM
@@ -64,10 +59,8 @@ cp ~/dot_file/vimrc ~/.vimrc
 # ---------- BUNDLE ---------- #
 vim -c 'BundleInstall!' -c 'qa!'
 
-echo "Success" | tee -a $LOG
 
-
-echo -e "\n$(date +%Y-%m-%d' '%T) \n# ========== TMUX ========== #" | tee -a $LOG
+echo "# ========== TMUX ========== #"
 
 # ---------- .TMUX.CONF  ----------
 if [[ $(tmux -V | sed 's/[^0-9]*//g') -ge "21" ]]; then
@@ -92,26 +85,22 @@ EOF
 fi
 # ---------- .TMUX.CONF COPY  ----------
 cat ~/dot_file/tmux.conf >> ~/.tmux.conf
-echo "Success" | tee -a $LOG
-echo -e "\n$(date +%Y-%m-%d' '%T) \n# ========== GIT BASH ========= #" | tee -a $LOG
+
+
+echo "# ========== GIT BASH ========= #"
 cat <<EOF>> ~/.bashrc
 GIT_PROMPT_THEME=Solarized
 GIT_PROMPT_ONLY_IN_REPO=1
 source $GIT_BASH/gitprompt.sh
 EOF
 
-echo "Success" | tee -a $LOG
 
-
-# ========== DONE ========== #
-echo "Done." | tee -a $LOG
-cat $LOG
+echo "# ========== ROOT ========== #"
 bash root_install.sh
 exit
-# ========== DONE ========== #
 
 
-echo -e "\n$(date +%Y-%m-%d' '%T) \n# ========== BASHRC PS1 ========== #" | tee -a $LOG
+echo "# ========== BASHRC PS1 ========== #" 
 # ---------- INSTANCE_NAME ----------
 cat <<EOF> ~/instance_name
 micro1
@@ -134,5 +123,3 @@ alias ll='echo \$(ls -Ahl | wc -l)'
 alias lu="ls -a"
 PS1='\[\033[00;31m\]\u\[\033[00m\]@\[\033[00;31m\]$(cat ~/instance_name):\[\033[00m\]\w\[\033[00;31m\]\$\[\033[0m\]\n'
 EOF
-echo "Success" | tee -a $LOG
-echo "Done."
